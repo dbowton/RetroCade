eefdd7674ab630a9db489813aa63938cc43ffbf7
AlanBuechner <47373257+AlanBuechner@users.noreply.github.com>
2022-02-10 11:16:51 -0700

    added database and started user login

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require("express");
 var path = require("path");
+const users = require("./users");
 
 const port = 3000;
 const app = express();
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 app.listen(port);
 
@@ -14,10 +15,11 @@ app.use("/css", express.static("css"));
 app.use("/js", express.static("js"));
 app.use("/images", express.static("images"));
 
-app.get(["/", "/login"], function(req, res){
+app.get(["/", "/login"], async function(req, res){
+	console.log(await users.validateUserLogin("alan", "1"));
 	res.render("login");
 });
 
-app.get("/register", function(req, res){
+app.get("/register", async function(req, res){
 	res.render("register");
 });
---
+++ users.js
@@ -0,0 +1,59 @@
+const mongoose = require("mongoose");
+const fs = require('fs');
+
+function readFile(filename)
+{
+	return JSON.parse(fs.existsSync(filename) ?
+	 fs.readFileSync(filename).toString() : '{}');
+}
+
+const dbInfo = readFile("db.json");
+
+const db = mongoose.connect(dbInfo.uri, { useNewUrlParser: true, useUnifiedTopology: true }).then(function(result)
+{
+	console.log("connected to db");
+}).catch(function(err)
+{
+	console.log("failed to connect to the db");
+	console.error(err);
+});
+
+const userSchema = mongoose.Schema({
+	username: String,
+	password: String,
+	email: String,
+	gameInfo: [{game:String, data:Object}]
+});
+
+const UserModel = mongoose.model("User", userSchema);
+
+async function addUser(username, password, email)
+{
+	const user = new UserModel({
+		username: username, 
+		password: password, 
+		email: email
+	});
+
+	const userInfo = await user.save();
+
+	return userInfo;
+}
+
+async function getUser(username)
+{
+	const users = await UserModel.find({username: username});
+	return users[0];
+}
+
+async function validateUserLogin(username, password)
+{
+	const user = await getUser(username);
+	return user.password === password;
+}
+
+module.exports = {
+	addUser,
+	getUser,
+	validateUserLogin
+};
