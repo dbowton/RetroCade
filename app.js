5312ceff9dd1e193a6f197fd19f6f3273d085bfa
AlanBuechner <47373257+AlanBuechner@users.noreply.github.com>
2022-02-14 09:01:27 -0700

    login

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 const express = require("express");
-var path = require("path");
+const cookieParser = require('cookie-parser');
 const users = require("./users");
-const crypto = require('crypto');
+const crypto = require("crypto");
 
 async function hash(text)
 {
-	return crypto.createHash('sha256').update(text).digest('hex');
+	return crypto.createHash("sha256").update(text).digest("hex");
+}
+
+async function redirectLogin(req, res, next)
+{
+	if(await users.checkSession(req.cookies.username))
+		next();
+	else
+		res.redirect("/login");
+	
 }
 
 const port = 3000;
@@ -16,7 +25,9 @@ app.listen(port);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
+// static 
 app.use("/css", express.static("css"));
 app.use("/js", express.static("js"));
 app.use("/images", express.static("images"));
@@ -28,10 +39,21 @@ app.get(["/", "/login"], async function(req, res){
 app.post("/login", async function(req, res){
 	const username = req.body.username;
 	const password = req.body.password;
+
+	if(await users.checkSession(req.cookies.username))
+	{
+		res.redirect("/home");
+		return;
+	}
+
 	const hashedPassword = await hash(password);
 
-	if(users.validateUserLogin(username, password))
-		res.redirect("/index");
+	if(users.validateUserLogin(username, hashedPassword))
+	{
+		await users.startSession(username);
+		res.cookie("username", username);
+		res.redirect("/home");
+	}
 	else
 		res.redirect("/login?error=error.auth.usernameOrEmailIncorect");
 	
@@ -68,11 +90,13 @@ app.post("/register", async function(req, res){
 	
 	// add user
 	users.addUser(username, hashedPassword, email);
-	res.redirect("/login");
+	users.startSession(username);
+	res.cookie("username", username);
+	res.redirect("/home");
 });
 
-app.get("/homepageTRIAL", async function(req, res){
-	res.render("homepageTRIAL");
+app.get("/home", redirectLogin, async function(req, res){
+	res.render("home");
 });
 
 // 404 error page
--- users.js
+++ users.js
@@ -1,15 +1,14 @@
 const mongoose = require("mongoose");
-const fs = require('fs');
+const fs = require("fs");
 
 function readFile(filename)
 {
 	return JSON.parse(fs.existsSync(filename) ?
-	 fs.readFileSync(filename).toString() : '{}');
+	 fs.readFileSync(filename).toString() : "{}");
 }
 
 const dbInfo = readFile("db.json");
-
-const db = mongoose.connect(dbInfo.uri, { useNewUrlParser: true, useUnifiedTopology: true }).then(function(result)
+mongoose.connect(dbInfo.uri, { useNewUrlParser: true, useUnifiedTopology: true }).then(function(result)
 {
 	console.log("connected to db");
 }).catch(function(err)
@@ -25,7 +24,14 @@ const userSchema = mongoose.Schema({
 	gameInfo: [{game:String, data:Object}]
 });
 
+const sessionSchema = mongoose.Schema({
+	sessionID: String,
+	username: String,
+	timeout: Date
+});
+
 const UserModel = mongoose.model("User", userSchema);
+const SessionModel = mongoose.model("Session", sessionSchema);
 
 async function addUser(username, password, email)
 {
@@ -38,6 +44,59 @@ async function addUser(username, password, email)
 	const userInfo = await user.save();
 
 	return userInfo;
+}
+
+async function startSession(username)
+{
+	if(await checkSession())
+		return await SessionModel.find({username: username});
+	
+	let timeoutTime = new Date(Date.now());
+	timeoutTime.setSeconds(timeoutTime.getSeconds() + 20000);
+
+	const session = new SessionModel({
+		username: username, 
+		sessionID: "",
+		timeout: timeoutTime
+	});
+
+	const sessionInfo = await session.save();
+
+	return sessionInfo;
+}
+
+async function removeSession(username)
+{
+	SessionModel.remove({username: username});
+}
+
+async function checkSession(username)
+{
+	const session = await SessionModel.find({username: username});
+	console.log(session);
+	if(!!session.length)
+	{
+		if(session.timeout > Date.now())
+		{
+			console.log("timeout");
+			removeSession(username);
+			return false;
+		}
+		else
+		{
+			console.log("renewing session");
+			let timeoutTime = new Date(Date.now());
+			timeoutTime.setSeconds(timeoutTime.getSeconds() + 20000);
+			session.timeout = timeoutTime;
+		}
+	}
+	else
+	{
+		console.log("no session found");
+		return false;
+	}
+
+	return true;
 }
 
 async function getUser(username)
@@ -55,10 +114,15 @@ async function getUserByEmail(email)
 async function validateUserLogin(username, password)
 {
 	const user = await getUser(username);
-	return user.password === password;
+	if(!user)
+		return false;
+	return password == user.password;
 }
 
 module.exports = {
+	startSession,
+	removeSession,
+	checkSession,
 	addUser,
 	getUser,
 	getUserByEmail,
