8529e7aaacc1b92b4e90b31ca989ad219c720cab
Alexgreano <alexisggr104@gmail.com>
2022-02-22 09:02:55 -0700

    added library, css, and example games to list

---
+++ js/CardRender.js
@@ -0,0 +1,30 @@
+$(document).ready(async function(){
+
+	var gamelist = (await getGameList()).games;
+
+	for(var i = 0; i < gamelist.length; i++){
+		const game = gamelist[i];
+		const html = `<div class="minusPadding">
+		<div class="row">
+			<div class="card cardSize" id="cardPadding">
+				<h4 class="title">${game.name}</h4>
+				
+				<div class="card-body side-by-side">
+					<img class="imgSize" src="~/css/pics/${game.image}" />
+					<div class="text-left">
+						<h6>Genre: Genre</h6>
+						<h6>Game Description: <p class="dSize">${game.description}</p>
+						</h6>
+					</div>
+				</div>
+			</div>
+		</div>`
+
+		$("#list").append(html);
+	}
+
+
+
+
+	
+})
