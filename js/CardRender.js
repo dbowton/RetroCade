f59542dd75323f5533c717758205e9d6ad4c38a0
Alexgreano <alexisggr104@gmail.com>
2022-02-23 09:00:52 -0700

    fixed link for games

--- js/CardRender.js
+++ js/CardRender.js
@@ -5,15 +5,15 @@ $(document).ready(async function(){
 	for(var i = 0; i < gamelist.length; i++){
 		const game = gamelist[i];
 		const html = `<div class="minusPadding">
-		<a href="/${game.name}">
+		<a href="/games/${game.name}">
 		<div class="row">
 			<div class="card cardSize" id="cardPadding">
 				<h4 class="title">${game.name}</h4>
 				
 				<div class="card-body side-by-side">
-					<img class="imgSize" src="~/css/pics/${game.image}" />
+					<img class="imgSize" src="css/pics/${game.image}" />
 					<div class="text-left">
-						<h6>Genre: Genre</h6>
+						<h6>Genre: ${game.genre}Genre</h6>
 						<h6>Game Description: <p class="dSize">${game.description}</p>
 						</h6>
 					</div>
