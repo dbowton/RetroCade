bdd3ca404d70c888953a6ac2cf7493c5c425d037
dbowton <80086095+dbowton@users.noreply.github.com>
2022-03-03 09:37:25 -0700

    Update to SpaceInvaders

--- js/spaceInvaders.js
+++ js/spaceInvaders.js
@@ -12,7 +12,7 @@ function setup()
 	ctx.fillText("Works Kinda", (c.width - ctx.measureText("Works Kinda").width) / 2, c.height / 2);
 }
 
-let player;
+let player = { x:20, y:c.width-c.width/3*2, w:50, h:30, color:"green", speed:4 }
 let gameData = 
 { 
     initEnemiesHorizontal:8, initEnemiesVerticle:6,
@@ -45,6 +45,32 @@ function start()
     enemiesVerticle = gameData.initEnemiesVerticle;
 
     makeEnemies(enemiesHorizontal, enemiesVerticle);
+}
+
+
+window.onkeydown = function(e)
+{
+	switch(e.keyCode)
+	{
+		case 65:
+		case 37:
+			player.x = Math.max(player.x - player.speed, gameData.screenBuffer);
+			break;
+		case 68:
+		case 39:
+			player.x = Math.min(player.x + player.speed, c.width - gameData.screenBuffer - player.w);
+			break;
+		case 32:
+			fire();
+			break;
+		default:
+			break;
+	}
+}
+
+function fire()
+{
+
 }
 
 function makeEnemies(verticle, horizontal)
@@ -100,19 +126,28 @@ function gameLoop()
 function draw()
 {
     ctx.clearRect(0, 0, c.width, c.height);
-    enemies.forEach(x => { drawBrick(x); });
+    enemies.forEach(x => { drawEnemy(x); });
+	drawPlayer();
 }
 
-function drawBrick(x)
+function drawPlayer()
+{
+	drawBrick(player.x, player.y, player.w, player.h, player.color);
+	drawBrick(player.x + player.w / 2 - player.w / 10, player.y - 10, player.w / 5, player.h, player.color);
+}
+
+function drawEnemy(x)
+{
+	drawBrick(x.x, x.y, x.w, x.h, x.color);
+//	drawBrick(x.x, x.y - 4 * x.h / 8, x.w / 11, x.h / 11 * 3, "green");
+//	drawBrick(x.x + x.w / 11, x.y - 4 * x.h / 8);
+}
+
+function drawBrick(x, y, w, h, color)
 {
     ctx.beginPath(); 
-	ctx.fillStyle = x.color; 
-	ctx.fillRect(x.x, x.y, x.w, x.h); 
-	ctx.stroke();
-	
-	ctx.beginPath(); 
-	ctx.fillStyle = "black"; 
-	ctx.rect(x.x, x.y, x.w, x.h); 
+	ctx.fillStyle = color; 
+	ctx.fillRect(x, y, w, h); 
 	ctx.stroke();
 }
 
@@ -124,7 +159,19 @@ function update()
 function updateEnemies()
 {
     let down = false;
-    enemies.forEach(x => { if(x.x + x.w > c.width - gameData.screenBuffer && !down){ enemySpeed = -enemySpeed; down = true;}});
+    enemies.forEach(x => 
+		{ 
+			if(enemySpeed > 0 && x.x + x.w > c.width - gameData.screenBuffer && !down)
+			{ 
+				enemySpeed = -enemySpeed; 
+				down = true;
+			}
+			else if(enemySpeed < 0 && x.x < gameData.screenBuffer && !down)
+			{
+				enemySpeed = -enemySpeed; 
+				down = true;
+			}
+		});
 
    if(down)
     {
