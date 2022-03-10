7e689022d168e38c260f3a5f7a250ddd49c6e443
Meliemau <hwhite@student.neumont.edu>
2022-03-09 19:30:29 -0700

    Fixed Coloring for Login and Search

--- js/CardRender.js
+++ js/CardRender.js
@@ -1,19 +1,19 @@
-$(document).ready(async function(){
+$(document).ready(async function () {
 	var gamelist = (await getGameList()).games;
 
-	for(var i = 0; i < gamelist.length; i++){
+	for (var i = 0; i < gamelist.length; i++) {
 		const game = gamelist[i];
 		const html = `<div class="minusPadding">
 		<a href="/games/${game.name}">
 		<div class="row">
 			<div class="card cardSize" id="cardPadding">
-				<h4 class="title">${game.name}</h4>
+				<h4 class="title" id="cardText">${game.name}</h4>
 				
 				<div class="card-body side-by-side">
 					<img class="imgSize" src="/images/${game.image}" />
 					<div class="text-left">
-						<h6>Genre: ${game.genre}</h6>
-						<h6>Game Description: <p class="dSize">${game.description}</p>
+						<h6 id="cardText">Genre: ${game.genre}</h6>
+						<h6 id="cardText">Game Description: <p class="dSize" id="cardText">${game.description}</p>
 						</h6>
 					</div>
 				</div>
--- js/casino.js
+++ js/casino.js
@@ -5,7 +5,7 @@
         Think of More Updates
 */
 
-var chips = 0;
+var chips = 100000000;
 
 var formatter = new Intl.NumberFormat('en-US', {
     style: 'currency',
@@ -20,19 +20,19 @@ var roulOwned = 0;
 
 var updatesOwned = 0;
 
-var lottoCost = 5;
-var slotsCost = 50;
-var pokerCost = 150;
+var lottoCost = 15;
+var slotsCost = 100;
+var pokerCost = 200;
 var bljkCost = 1250;
 var roulCost = 5000;
 
 var updateCost = 25000;
 
-var lottoMul = 2;
-var slotsMul = 2;
-var pokerMul = 2;
-var bljkMul = 3;
-var roulMul = 3;
+var lottoMul = 5;
+var slotsMul = 15;
+var pokerMul = 25;
+var bljkMul = 50;
+var roulMul = 100;
 
 setInterval(theCounter, 1000);
 setInterval(theUpdate, 100);
@@ -46,19 +46,19 @@ function theCounter() {
     }
     if (slotsOwned > 0) {
         chips++;
-        chips += (3 * slotsOwned);
+        chips += (15 * slotsOwned);
     }
     if (pokerOwned > 0) {
         chips++;
-        chips += (5 * pokerOwned);
+        chips += (100 * pokerOwned);
     }
     if (bljkOwned > 0) {
         chips++;
-        chips += (7 * bljkOwned);
+        chips += (10 * bljkOwned);
     }
     if (roulOwned > 0) {
         chips++;
-        chips += (9 * roulOwned);
+        chips += (15 * roulOwned);
     }
 
 }
@@ -69,21 +69,21 @@ function theUpdate() {
     //update the Money and cost of each item
     document.getElementById("Money").innerHTML = chipsFormatted;
 
-    document.getElementById("btn").innerHTML = "Lotto! $" + lottoCost + "\nOwned: " + lottoOwned + " Makes: $" + (1 * (lottoOwned + 1)) + "/s";
+    document.getElementById("btn").innerHTML = "Lotto! " + formatter.format(lottoCost) + "\nOwned: " + lottoOwned + " Makes: $" + (1 * (lottoOwned + 1)) + "/s";
 
-    document.getElementById("btn1").innerHTML = "Slots! $" + slotsCost + "\nOwned: " + slotsOwned + " Makes: $" + (3 * (slotsOwned + 1)) + "/s";
+    document.getElementById("btn1").innerHTML = "Slots! " + formatter.format(slotsCost) + "\nOwned: " + slotsOwned + " Makes: $" + (15 * (slotsOwned + 1)) + "/s";
 
-    document.getElementById("btn2").innerHTML = "Poker! $" + pokerCost + "\nOwned: " + pokerOwned + " Makes: $" + (5 * (pokerOwned + 1)) + "/s";
+    document.getElementById("btn2").innerHTML = "Poker! " + formatter.format(pokerCost) + "\nOwned: " + pokerOwned + " Makes: $" + (100 * (pokerOwned + 1)) + "/s";
 
-    document.getElementById("btn3").innerHTML = "BlackJack! $" + bljkCost + "\nOwned: " + bljkOwned + " Makes: $" + (7 * (bljkOwned + 1)) + "/s";
+    document.getElementById("btn3").innerHTML = "BlackJack! " + formatter.format(bljkCost) + "\nOwned: " + bljkOwned + " Makes: $" + (10 * (bljkOwned + 1)) + "/s";
 
-    document.getElementById("btn4").innerHTML = "Roulette! $" + roulCost + "\nOwned: " + roulOwned + " Makes: $" + (9 * (roulOwned + 1)) + "/s";
+    document.getElementById("btn4").innerHTML = "Roulette! " + formatter.format(roulCost) + "\nOwned: " + roulOwned + " Makes: $" + (15 * (roulOwned + 1)) + "/s";
 
     if (updatesOwned == 0) {
-        document.getElementById("btn5").innerHTML = "Update! $" + updateCost + " New Idle Machines";
+        document.getElementById("btn5").innerHTML = "Update! " + formatter.format(updateCost) + " New Idle Machines";
     }
     if (updatesOwned == 1) {
-        document.getElementById("btn5").innerHTML = "Update! $" + updateCost + " New Carpet";
+        document.getElementById("btn5").innerHTML = "Update! " + formatter.format(updateCost) + " New Carpet";
     }
     if (updatesOwned == 2) {
         document.getElementById("btn5").innerHTML = "Update! $" + updateCost + " New Location";
@@ -150,6 +150,8 @@ function theUpdate() {
         document.getElementById("btn5").setAttribute("disabled", "disabled");
     }
 
+
+
 }
 
 function chipClicked() {
@@ -162,9 +164,42 @@ function lottoClicked() {
         lottoOwned++;
         chips -= lottoCost;
 
-        lottoCost *= lottoMul;
+        if (lottoOwned <= 5) {
+            lottoCost += lottoMul;
+        }
 
-        lottoMul++;
+        if (lottoOwned == 9) {
+            lottoMul += 8;
+        }
+
+        if (lottoOwned <= 10 && lottoOwned > 5) {
+            lottoCost += lottoMul;
+        }
+
+        if (lottoOwned == 14) {
+            lottoMul += 10;
+        }
+
+        if (lottoOwned <= 15 && lottoOwned > 10) {
+            lottoCost += lottoMul;
+        }
+
+        if (lottoOwned == 19) {
+            lottoMul += 20;
+        }
+
+        if (lottoOwned <= 20 && lottoOwned > 15) {
+            lottoCost += lottoMul;
+        }
+
+        if (lottoOwned == 21) {
+            lottoMul = 3;
+        }
+
+        if (lottoOwned >= 21 && lottoOwned > 20) {
+            lottoCost = (lottoCost * lottoMul) / 2;
+        }
+
 
         document.getElementById('lotto').style.display = 'block';
     }
@@ -175,9 +210,41 @@ function slotsClicked() {
         slotsOwned++;
         chips -= slotsCost;
 
-        slotsCost *= slotsMul;
+        if (slotsOwned <= 5) {
+            slotsCost += slotsMul;
+        }
 
-        slotsMul++;
+        if (slotsOwned == 4) {
+            slotsMul += 35;
+        }
+
+        if (slotsOwned <= 10 && slotsOwned > 5) {
+            slotsCost += slotsMul;
+        }
+
+        if (slotsOwned == 9) {
+            slotsMul += 45;
+        }
+
+        if (slotsOwned <= 15 && slotsOwned > 10) {
+            slotsCost += slotsMul;
+        }
+
+        if (slotsOwned == 14) {
+            slotsMul += 100;
+        }
+
+        if (slotsOwned <= 20 && slotsOwned > 15) {
+            slotsCost += slotsMul;
+        }
+
+        if (slotsOwned == 21) {
+            slotsMul = 4;
+        }
+
+        if (slotsOwned >= 21 && slotsOwned > 20) {
+            slotsCost = (slotsCost * slotsMul) / 3;
+        }
 
         document.getElementById('slots').style.display = 'block';
     }
@@ -188,9 +255,41 @@ function pokerClicked() {
         pokerOwned++;
         chips -= pokerCost;
 
-        pokerCost *= pokerMul;
+        if (pokerOwned <= 5) {
+            pokerCost += pokerMul;
+        }
 
-        pokerMul++;
+        if (lottoOwned == 9) {
+            pokerMul += 50;
+        }
+
+        if (pokerOwned <= 10 && pokerOwned > 5) {
+            pokerCost += pokerMul;
+        }
+
+        if (pokerOwned == 14) {
+            pokerMul += 15;
+        }
+
+        if (pokerOwned <= 15 && pokerOwned > 10) {
+            pokerCost += pokerMul;
+        }
+
+        if (pokerOwned == 19) {
+            pokerMul += 25;
+        }
+
+        if (pokerOwned <= 20 && pokerOwned > 15) {
+            pokerCost += pokerMul;
+        }
+
+        if (pokerOwned == 21) {
+            pokerMul = 3;
+        }
+
+        if (pokerOwned >= 21 && pokerOwned > 20) {
+            pokerCost = (pokerCost * pokerMul) / 2;
+        }
 
         document.getElementById('poker').style.display = 'block';
     }
@@ -201,9 +300,41 @@ function bljkClicked() {
         bljkOwned++;
         chips -= bljkCost;
 
-        bljkCost *= bljkMul;
+        if (bljkOwned <= 5) {
+            bljkCost += bljkMul;
+        }
 
-        bljkMul++;
+        if (bljkOwned == 4) {
+            bljkMul += 50;
+        }
+
+        if (bljkOwned <= 10 && bljkOwned > 5) {
+            bljkCost += bljkMul;
+        }
+
+        if (bljkOwned == 9) {
+            bljkMul += 75;
+        }
+
+        if (bljkOwned <= 15 && bljkOwned > 10) {
+            bljkCost += bljkMul;
+        }
+
+        if (bljkOwned == 14) {
+            bljkMul += 100;
+        }
+
+        if (bljkOwned <= 20 && bljkOwned > 15) {
+            bljkCost += bljkMul;
+        }
+
+        if (bljkOwned == 21) {
+            bljkMul = 4;
+        }
+
+        if (bljkOwned >= 21 && bljkOwned > 20) {
+            bljkCost = (bljkCost * bljkMul) / 3;
+        }
 
         document.getElementById('bljk').style.display = 'block';
     }
@@ -214,9 +345,41 @@ function roulClicked() {
         roulOwned++;
         chips -= roulCost;
 
-        roulCost *= roulMul;
+        if (roulOwned <= 5) {
+            roulCost += roulMul;
+        }
 
-        roulMul++;
+        if (roulOwned == 4) {
+            roulMul += 75;
+        }
+
+        if (roulOwned <= 10 && roulOwned > 5) {
+            roulCost += roulMul;
+        }
+
+        if (roulOwned == 9) {
+            roulMul += 100;
+        }
+
+        if (roulOwned <= 15 && roulOwned > 10) {
+            roulCost += roulMul;
+        }
+
+        if (roulOwned == 14) {
+            roulMul += 150;
+        }
+
+        if (roulOwned <= 20 && roulOwned > 15) {
+            roulCost += roulMul;
+        }
+
+        if (roulOwned == 21) {
+            roulMul = 4;
+        }
+
+        if (roulOwned >= 21 && roulOwned > 20) {
+            roulCost = (roulCost * roulMul) / 3;
+        }
 
         document.getElementById('roul').style.display = 'block';
     }
@@ -264,10 +427,4 @@ function updateClicked() {
             return;
         }
     }
-}
-
-function munnyError(cost) {
-    console.log("Not enough muns, Needed: " + cost);
-
-    document.getElementById("messageBoard").innerHTML = "Not enough Chips, Need: " + cost;
 }
